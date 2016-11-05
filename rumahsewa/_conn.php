95c7fd2a081603277677f7e511aece45490f7b9a
irfanfikry <irfanfikry94@gmail.com>
2016-11-05 20:19:35 +0800

    Update _conn.php

--- rumahsewa/_conn.php
+++ rumahsewa/_conn.php
@@ -5,7 +5,7 @@ function dbConnect()
     try
 		{
             $username = 'root'; //Change to your username for phpmyadmin
-            $userpassword = ''; //Please enter your password for phpmyadmin
+            $userpassword = 'fakhrupi'; //Please enter your password for phpmyadmin
             $conn = new pdo("mysql:host=localhost;dbname=rumahsewa;", $username, $userpassword); 
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
